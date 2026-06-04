70a8af3d0a9c84dbb8be377c1caf38dc37e66aa9
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2026-06-04 22:13:12 +0200

    chore: update julweb image to v1.1.0

--- colmena/modules/nixos/modules/containers/julweb/default.nix
+++ colmena/modules/nixos/modules/containers/julweb/default.nix
@@ -20,7 +20,7 @@ in
 
     image = mkOption {
       type = types.str;
-      default = "ghcr.io/quadrubo/julweb:v1.0.0"; # renovate: docker
+      default = "ghcr.io/quadrubo/julweb:v1.1.0"; # renovate: docker
     };
 
     domain = mkOption {
